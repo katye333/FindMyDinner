b3d9f1aa2ead0c75ff031cc3d3f9e87ebd2f8c9f
Katye Stevens <katye333@gmail.com>
2022-05-25 18:06:02 -0400

    feat: Setup authentication service and context

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
+import React, { useState, useEffect } from "react";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import { ThemeProvider } from "styled-components/native";
-import {useFonts as useOswald, Oswald_400Regular } from "@expo-google-fonts/oswald";
+import { useFonts as useOswald, Oswald_400Regular } from "@expo-google-fonts/oswald";
 import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 import { theme } from "./src/infrastructure/theme";
 
 import { RestaurantsContextProvider } from "./src/services/restaurants/restaurants.context";
 import { LocationContextProvider } from "./src/services/location/location.context";
 import { FavoritesContextProvider } from "./src/services/favorites/favorites.context";
+import { AuthenticationContextProvider } from "./src/services/authentication/authentication.context";
 import { Navigation } from "./src/infrastructure/navigation";
 
 import * as firebase from "firebase";
@@ -17,9 +19,11 @@ const firebaseConfig = {
     storageBucket: "mealstogo-meow.appspot.com",
     messagingSenderId: "849063461590",
     appId: "1:849063461590:web:86af9df7e12b66ca8bff77"
-  };
+};
 
-firebase.initializeApp(firebaseConfig);
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
 
 export default function App() {
     const [oswaldLoaded] = useOswald({
@@ -37,13 +41,15 @@ export default function App() {
     return (
         <>
             <ThemeProvider theme={theme}>
-                <FavoritesContextProvider>
-                <LocationContextProvider>
-                    <RestaurantsContextProvider>
-                        <Navigation />
-                    </RestaurantsContextProvider>
-                    </LocationContextProvider>
-                </FavoritesContextProvider>
+                <AuthenticationContextProvider>
+                    <FavoritesContextProvider>
+                    <LocationContextProvider>
+                        <RestaurantsContextProvider>
+                            <Navigation />
+                        </RestaurantsContextProvider>
+                        </LocationContextProvider>
+                    </FavoritesContextProvider>
+                </AuthenticationContextProvider>
             </ThemeProvider>
             <ExpoStatusBar style="auto" />
         </>
---
+++ src/services/authentication/authentication.service.js
@@ -0,0 +1,6 @@
+import * as firebase from "firebase";
+
+const loginRequest = (email, password) => {
+    firebase.auth().signInWithEmailAndPassword(email, password);
+
+}

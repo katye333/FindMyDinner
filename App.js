360cc4ca81dd0339b4b3b35ce8b2440df32fadf8
Katye Stevens <katye333@gmail.com>
2022-05-24 20:04:28 -0400

    feat: Setup firebase config - Downgrade pkg to 8.10.0

--- App.js
+++ App.js
@@ -9,6 +9,18 @@ import { LocationContextProvider } from "./src/services/location/location.contex
 import { FavoritesContextProvider } from "./src/services/favorites/favorites.context";
 import { Navigation } from "./src/infrastructure/navigation";
 
+import * as firebase from "firebase";
+const firebaseConfig = {
+    apiKey: "AIzaSyC6Rk2OR28z3u4vDGcMrWJ_IpO6WWpMMb8",
+    authDomain: "mealstogo-meow.firebaseapp.com",
+    projectId: "mealstogo-meow",
+    storageBucket: "mealstogo-meow.appspot.com",
+    messagingSenderId: "849063461590",
+    appId: "1:849063461590:web:86af9df7e12b66ca8bff77"
+  };
+
+firebase.initializeApp(firebaseConfig);
+
 export default function App() {
     const [oswaldLoaded] = useOswald({
         Oswald_400Regular,

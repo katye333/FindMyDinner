583e7bfca1a095e9c2b1b9b3af22381dc5477be1
Katye Stevens <katye333@gmail.com>
2022-06-04 15:26:55 -0400

    feat: Move mock locations to a firebase function

--- src/services/location/location.service.js
+++ src/services/location/location.service.js
@@ -1,15 +1,7 @@
 import camelize from "camelize";
-import { locations } from "./location.mock";
-
 
 export const locationRequest = (searchTerm) => {
-    return new Promise((resolve, reject) => {
-        const locationMock = locations[searchTerm];
-        if (!locationMock) {
-            reject('Not Found');
-        }
-        resolve(locationMock);
-    });
+    
 };
 
 

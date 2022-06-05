d84a03c9597ff1ee280f7e9e595181706591b4fd
Katye Stevens <katye333@gmail.com>
2022-06-05 15:58:33 -0400

    feat: Set up Google Node API

--- functions/geocode/index.js
+++ functions/geocode/index.js
@@ -1,9 +1,23 @@
 const { locations: locationsMock } = require("./geocode.mock");
 const url = require("url");
 
-module.exports.geocodeRequest = (request, response) => {
-    const { city } = url.parse(request.url, true).query;
-    const locationMock = locationsMock[city.toLowerCase()];
+module.exports.geocodeRequest = (request, response, client) => {
+    const { city, mock } = url.parse(request.url, true).query;
 
-    response.json(locationMock);
+    if (mock === "true") {
+        const locationMock = locationsMock[city.toLowerCase()];
+        return response.json(locationMock);
+    }
+    
+    client.geocode({
+        params: {
+            address: city,
+        },
+        timeout: 1000,
+    }).then( res => {
+        return response.json(res.data);
+    }).catch(error => {
+        response.status(400);
+        return response.send(error.response.data.error_message);
+    })
 };
--- src/utils/env.js
+++ src/utils/env.js
@@ -1,7 +1,7 @@
 const liveHost = 'https://us-central1-mealstogo-meow.cloudfunctions.net';
 
 // localHost changes every time due to port forwarding
-const localHost = 'https://8e12-72-184-186-170.ngrok.io/mealstogo-meow/us-central1';
+const localHost = 'https://0281-72-184-186-170.ngrok.io/mealstogo-meow/us-central1';
 
 
 export const isDevelopment = process.env.NODE_ENV === 'development';

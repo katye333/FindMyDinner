065cec6bddc072bfe725f598d5b8203b8a536dd9
Katye Stevens <katye333@gmail.com>
2022-06-05 16:12:46 -0400

    feat: Add (hidden) google api key and remove unneeded file tracking

--- functions/geocode/index.js
+++ functions/geocode/index.js
@@ -1,5 +1,6 @@
 const { locations: locationsMock } = require("./geocode.mock");
 const url = require("url");
+const functions = require("firebase-functions");
 
 module.exports.geocodeRequest = (request, response, client) => {
     const { city, mock } = url.parse(request.url, true).query;
@@ -12,6 +13,7 @@ module.exports.geocodeRequest = (request, response, client) => {
     client.geocode({
         params: {
             address: city,
+            key: functions.config().google.key,
         },
         timeout: 1000,
     }).then( res => {

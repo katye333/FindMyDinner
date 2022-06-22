c5a0fd8495f7d7f2234442efc4d01301fddd2a41
Katye Stevens <katye333@gmail.com>
2022-06-22 18:15:01 -0400

    style: Add background image to settings screen

--- src/features/settings/screens/settings.screen.js
+++ src/features/settings/screens/settings.screen.js
@@ -1,10 +1,9 @@
 import React, { useContext, useState, useCallback } from "react";
-import { TouchableOpacity } from "react-native";
+import { TouchableOpacity, SafeAreaView, StatusBar } from "react-native";
 import styled from "styled-components/native";
 import { List, Avatar } from "react-native-paper";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-import { SafeArea } from "../../../components/utility/safe-area.component";
 import { AuthenticationContext } from "../../../services/authentication/authentication.context";
 
 import { Text } from "../../../components/typography/text.component";
@@ -12,8 +11,23 @@ import { Spacer } from "../../../components/spacer/spacer.component";
 import { useFocusEffect } from "@react-navigation/native";
 import { colors } from "../../../infrastructure/theme/colors";
 
+const SettingsBackground = styled.ImageBackground.attrs({
+    source: require('../../../../assets/home_bg.jpg')
+})`
+    position: absolute;
+    height: 100%;
+    width: 100%;
+`;
+
+const TransparentSafeArea = styled(SafeAreaView)`
+    flex: 1;
+    ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
+    background-color: transparent;
+`;
+
 const SettingsItem = styled(List.Item)`
     padding: ${props => props.theme.space[3]};
+    background-color: rgba(255,255,255,0.4)
 `;
 const AvatarContainer = styled.View`
     align-items: center;
@@ -34,36 +48,51 @@ export const SettingsScreen = ({ navigation}) => {
     );
 
     return (
-        <SafeArea>
-            <AvatarContainer>
-                <TouchableOpacity onPress={() => navigation.navigate("Camera")}>
-                    {
-                        !photo &&
-                        <Avatar.Icon size={180} icon="human" backgroundColor={colors.brand.primary} />
-                    }
-                    {
-                        photo &&
-                        <Avatar.Image size={180} source={{uri: photo}} backgroundColor="#2182BD" />
-                    }
-                </TouchableOpacity>
+        <SettingsBackground>
+            <TransparentSafeArea>
+                <AvatarContainer>
+                    <TouchableOpacity onPress={() => navigation.navigate("Camera")}>
+                        {
+                            !photo &&
+                            <Avatar.Icon size={180} icon="human" backgroundColor={colors.brand.primary} />
+                        }
+                        {
+                            photo &&
+                            <Avatar.Image size={180} source={{uri: photo}} backgroundColor="#2182BD" />
+                        }
+                    </TouchableOpacity>
 
-                <Spacer position={"top"} size={"large"}>
-                    <Text variant="label">{user.email}</Text>
-                </Spacer>
-            </AvatarContainer>
-
-            <List.Section>
-                <SettingsItem
-                    title="View Your Favorites"
-                    left={(props) => <List.Icon {...props} color="black" icon="heart" />}
-                    onPress={() => navigation.navigate("Favorites")}
-                />
-                <SettingsItem 
-                    title="Logout"
-                    left={(props) => <List.Icon {...props} color="black" icon="door" />}
-                    onPress={onLogout}
-                />
-            </List.Section>
-        </SafeArea>
+                    <Spacer position={"top"} size={"large"}>
+                        <Text variant="label">{user.email}</Text>
+                    </Spacer>
+                </AvatarContainer>
+                <Spacer position={"top"} size={"large"} />
+                <List.Section>
+                    <SettingsItem
+                        title="View Your Favorites"
+                        left={(props) => <List.Icon {...props} color={colors.ui.error} icon="heart" />}
+                        onPress={() => navigation.navigate("Favorites")}
+                    />
+                    <Spacer />
+                    <SettingsItem 
+                        title="Payment"
+                        left={(props) => <List.Icon {...props} color={colors.ui.secondary} icon="cart" />}
+                        onPress={() => null}
+                    />
+                    <Spacer />
+                    <SettingsItem 
+                        title="Past Orders"
+                        left={(props) => <List.Icon {...props} color={colors.ui.secondary} icon="history" />}
+                        onPress={() => null}
+                    />
+                    <Spacer />
+                    <SettingsItem 
+                        title="Logout"
+                        left={(props) => <List.Icon {...props} color={colors.ui.secondary} icon="door" />}
+                        onPress={onLogout}
+                    />
+                </List.Section>
+            </TransparentSafeArea>
+        </SettingsBackground>
     );
 }

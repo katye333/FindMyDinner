f5243520920af0f022e528a9004a9f5dd0c9c1ef
Katye Stevens <katye333@gmail.com>
2022-06-26 12:45:54 -0400

    style: Make the transparency less transparent with new background

--- src/features/settings/screens/settings.screen.js
+++ src/features/settings/screens/settings.screen.js
@@ -27,7 +27,7 @@ const TransparentSafeArea = styled(SafeAreaView)`
 
 const SettingsItem = styled(List.Item)`
     padding: ${props => props.theme.space[3]};
-    background-color: rgba(255,255,255,0.4)
+    background-color: rgba(255,255,255,0.8);
 `;
 const AvatarContainer = styled.View`
     align-items: center;
